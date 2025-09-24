74c4ffd06b770b39952051881adfe8676a92608a
MUHAMMED HASHIR <139986781+hazecodez@users.noreply.github.com>
2025-09-24 15:31:47 +0530

    Update webflow.js in api folder

--- api/webflow.js
+++ api/webflow.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const secret = req.headers["webflow-signature"]; 
 
-    if (secret !== process.env.WEBFLOW_SECRET_KEY) {
+    if (secret !== ea6c88be8c0ec3257007bf71410f08cd9ab9a1487377a7f4a0e21803063c086c) {
       return res.status(401).json({ error: "Invalid secret" });
     }
 
